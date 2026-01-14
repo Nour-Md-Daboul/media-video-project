1bd30d56252e8991f74c3710f4690b59ae2ce909
Nour-Md-Daboul <khalednour1612025@gmail.com>
2026-01-14 22:39:34 +0300

    تعديل الهيكلية

--- video_project/settings.py
+++ video_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'player',
+    
 ]
 
 MIDDLEWARE = [
@@ -117,15 +117,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
