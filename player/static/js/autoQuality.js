d195558ba38e0b56b5963698ce26f92718657c55
raneem387 <r25885002@gmail.com>
2026-01-14 23:28:48 +0300

    add autoQuality

---
+++ player/static/js/autoQuality.js
@@ -0,0 +1,28 @@
+
+//  Auto Quality Engine
+
+export function chooseQuality(speed, qualities) {
+
+  if (speed < 2 && qualities["144p"])
+    return { q: "144p", url: qualities["144p"] };
+
+  if (speed < 6 && qualities["360p"])
+    return { q: "360p", url: qualities["360p"] };
+
+  if (qualities["720p"])
+    return { q: "720p", url: qualities["720p"] };
+
+  const first = Object.keys(qualities)[0];
+  return { q: first, url: qualities[first] };
+}
+
+export function autoPlay(videoElement, speed, qualities) {
+  const selected = chooseQuality(speed, qualities);
+
+  videoElement.src = selected.url;
+  videoElement.setAttribute("data-quality", selected.q);
+  videoElement.load();
+  videoElement.play();
+
+  console.log(" Selected Quality:", selected.q);
+}
