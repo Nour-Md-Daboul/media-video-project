64e99917ebba2b24900a80f460c6ee7a5d4aa028
Nour-Md-Daboul <khalednour1612025@gmail.com>
2026-01-14 21:10:07 +0300

    واجهة الفيديو

--- player/views.py
+++ player/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def index(request):
+    return render(request, 'player/index.html')
+
+def video_qualities(request):
+    return JsonResponse({
+        "qualities": [
+            {"quality": 144, "url": "/media/videos/video_144.mp4"},
+            {"quality": 360, "url": "/media/videos/video_360.mp4"},
+            {"quality": 720, "url": "/media/videos/video_720.mp4"},
+        ]
+    })
